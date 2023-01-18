cf5e803e6a6d36d1bc4460548225e83f341f2e0f
INMD1 <87979171+INMD1@users.noreply.github.com>
2023-01-19 00:32:15 +0900

    non

--- app/src/main/java/com/inmd1/deu_food_gui/MainActivity.kt
+++ app/src/main/java/com/inmd1/deu_food_gui/MainActivity.kt
@@ -18,19 +18,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        webView = findViewById(R.id.webview)
 
-        webView.apply {
-            webViewClient = WebViewClient()
-
-            settings.javaScriptEnabled = true
-            settings.setSupportMultipleWindows(false)
-            settings.useWideViewPort = true
-            settings.setSupportZoom(false)
-            settings.builtInZoomControls = false
-        }
-        val uri = "https://mobile.udon.party/"
-        webView.loadUrl(uri)
     }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
